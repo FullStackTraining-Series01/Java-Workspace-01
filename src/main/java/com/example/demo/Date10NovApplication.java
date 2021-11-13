b6f4911cc6c404c766419d0e65853245a2199543
AjayOnlineTrainer <91732436+AjayOnlineTrainer@users.noreply.github.com>
2021-11-13 06:44:19 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/Date10NovApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Date10NovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Date10NovApplication.class, args);
+	}
+
+}
